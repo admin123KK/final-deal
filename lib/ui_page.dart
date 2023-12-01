a6f4a493a0b4b4f4559b88a1a4afe7597c3241ed
admin123KK <karkiaku000@gmail.com>
2023-12-01 18:40:42 +0545

    something better in row column

--- lib/ui_page.dart
+++ lib/ui_page.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 enum MenuAction { logout }
 
@@ -15,107 +14,62 @@ class _UiPageState extends State<UiPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color(0xFF00563B),
-          title: const Text(
-            'Ui Page',
-            style: TextStyle(color: Colors.white),
-          ),
-          actions: [
-            PopupMenuButton<MenuAction>(
-              onSelected: (value) async {
-                switch (value) {
-                  case MenuAction.logout:
-                    final shouldLogout = await showLogOutDialog(context);
-                    if (shouldLogout) {
-                      await FirebaseAuth.instance.signOut();
-
-                      Navigator.of(context)
-                          .pushNamedAndRemoveUntil('/LoginPage', (_) => false);
-                    }
-                }
-              },
-              itemBuilder: (context) {
-                return [
-                  PopupMenuItem<MenuAction>(
-                    value: MenuAction.logout,
-                    child: const Text('Logout'),
-                  )
-                ];
-              },
-            )
-          ],
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF00563B),
+        title: const Text(
+          'Ui Page',
+          style: TextStyle(color: Colors.white),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(20),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 30,
-                ),
-                const Row(
-                  children: [
-                    Text('Namste'),
-                  ],
-                ),
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: [
-                      Container(
-                        width: 300,
-                        height: 300,
-                        color: Colors.green,
-                        padding: const EdgeInsets.all(20),
-                        margin: const EdgeInsets.all(10),
-                      ),
-                      Row(
-                        children: [
-                          Container(
-                            width: 300,
-                            height: 300,
-                            color: Colors.green,
-                            padding: const EdgeInsets.all(20),
-                            margin: const EdgeInsets.all(10),
-                          )
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Container(
-                            width: 300,
-                            height: 300,
-                            color: Colors.green,
-                            padding: const EdgeInsets.all(20),
-                            margin: const EdgeInsets.all(10),
-                          )
-                        ],
-                      ),
-                    ],
+        actions: [
+          PopupMenuButton<MenuAction>(
+            onSelected: (value) async {
+              switch (value) {
+                case MenuAction.logout:
+                  final shouldLogout = await showLogOutDialog(context);
+                  if (shouldLogout) {
+                    await FirebaseAuth.instance.signOut();
+
+                    Navigator.of(context)
+                        .pushNamedAndRemoveUntil('/LoginPage', (_) => false);
+                  }
+              }
+            },
+            itemBuilder: (context) {
+              return [
+                PopupMenuItem<MenuAction>(
+                  value: MenuAction.logout,
+                  child: const Text('Logout'),
+                )
+              ];
+            },
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: 250,
+                width: 250,
+                alignment: Alignment.topCenter,
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.grey[700],
+                  borderRadius: BorderRadius.circular(25.0),
+                  image: const DecorationImage(
+                    image: AssetImage('assets/sky.image.png'),
+                    fit: BoxFit.cover,
                   ),
                 ),
-                SizedBox(
-                  height: 20,
-                ),
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: [
-                      Container(
-                        width: 300,
-                        height: 300,
-                        color: Colors.green,
-                        padding: const EdgeInsets.all(20),
-                        margin: const EdgeInsets.all(10),
-                      )
-                    ],
-                  ),
-                ),
-              ],
-            ),
+              child: const Text('Namaste',style: TextStyle(color: Colors.white,fontFamily: 'Mooli',fontWeight: FontWeight.bold),),
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
 
